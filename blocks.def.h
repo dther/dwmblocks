b9f74e97edd767e77ec052edeef40578bb97aba6
Rudy Dellomas III <rdellomasiii@outlook.com>
2021-01-17 00:13:20 +1000

    Add my modules and allow my extra long boy, mpd.sh

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,17 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"",	"packages.sh",	0,	1},
+        {"",    "wifi.sh",  0,  2},
+        {"",    "battery.sh", 5, 3},
+        {"",    "datetime.sh", 5, 4},
+        {";",    "", 0, NULL},
+        {"",    "volume.sh", 0, 5},
+        {"",    "memory.sh", 10, 6},
+        {"",    "email.sh", 60, 7},
+        {"",    "mpd.sh", 1, 8},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static char delim[] = " ";
+static unsigned int delimLen = 1;
